8729a8dcb5eab929e3000ddcfda77fcc25cde051
Задворний Максим Сергійович <maksiksking@gmail.com>
2023-06-28 16:02:10 +0300

    v.sound

--- scrip.js
+++ scrip.js
@@ -1,23 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    //Carssification chamber for tv1
-
     const leftOne = document.getElementsByClassName("leftOne")[0];
     const rightOne = document.getElementsByClassName("rightOne")[0];
     const carcass = document.getElementsByClassName("carcass1")[0];
-
-    function carcassify() {
-        const carcassHeight = carcass.offsetHeight;
-
-        leftOne.style.height = carcassHeight + "px";
-        rightOne.style.height = carcassHeight + "px";
-        console.log(leftOne.style.height = carcassHeight + "px")
-    }
-
-    window.addEventListener('load', carcassify)
-    addEventListener("resize", carcassify);
-
-    //Carssification chamber for tv2
 
     const github = document.getElementsByClassName("github")[0];
     const leftCoding = document.getElementsByClassName("leftCoding")[0];
@@ -27,22 +12,119 @@ document.addEventListener("DOMContentLoaded", function () {
     const pancakeBottom = document.getElementsByClassName("pancakeBottom")[0];
     const carcass2 = document.getElementsByClassName("carcass2")[0];
 
-    function carcassify2() {
+    const gallery = document.getElementsByClassName("tv3")[0];
+    const modal = document.getElementsByClassName("modal")[0];
+    const modalImg = document.getElementsByClassName("modalImg")[0];
+
+    const folderBg = document.getElementsByClassName("folderBg")[0];
+    const folderBgPix = document.getElementsByClassName("folderBg")[1];
+    const folderBgAI = document.getElementsByClassName("folderBg")[2];
+    const designBg = document.getElementsByClassName("imageGallery")[0];
+    const designBgPix = document.getElementsByClassName("imageGalleryPix")[0];
+    const designBgAI = document.getElementsByClassName("imageGalleryAI")[0];
+    const leftFolder = document.getElementsByClassName("leftFolder")[0];
+    const leftFolderPix = document.getElementsByClassName("leftFolder")[1];
+    const leftFolderAI = document.getElementsByClassName("leftFolder")[2];
+    const rightFolder = document.getElementsByClassName("rightFolder")[0];
+    const rightFolderPix = document.getElementsByClassName("rightFolder")[1];
+    const rightFolderAI = document.getElementsByClassName("rightFolder")[2];
+
+    const blankReal = document.getElementsByClassName("blank")[0];
+    const blankReal1 = document.getElementsByClassName("blank")[1];
+
+    const designTxt = document.getElementsByClassName("designTxt")[0];
+
+    let blankPlaceholder;
+
+    function carcassify123() {
+        const carcassHeight = carcass.offsetHeight;
         const carcassHeight2 = carcass2.offsetHeight;
 
+        leftOne.style.height = carcassHeight + "px";
+        rightOne.style.height = carcassHeight + "px";
+
         github.style.height = carcassHeight2 + "px";
-
         leftCoding.style.height = carcassHeight2 + "px";
-
         coding.style.height = carcassHeight2 + "px";
-
         pancakeTop.style.height = carcassHeight2 + "px";
-
         pancakeMiddle.style.height = carcassHeight2 + "px";
-
         pancakeBottom.style.height = carcassHeight2 + "px";
+
+        gallery.style.height = carcassHeight2 + "px";
+        console.log(blankReal + "aaa");
+        console.log(document.getElementById("blankReal") + "aaaa");
     }
 
-    window.addEventListener('load', carcassify2)
-    addEventListener("resize", carcassify2);
+    window.addEventListener('load', carcassify123)
+    addEventListener("resize", carcassify123);
+
+    function modalAppear(evt) {
+        console.log(blankReal + "aa")
+        if (evt.target === folderBg  || evt.target === folderBgPix || evt.target === designBg || evt.target === designBgPix
+            || evt.target === designBgAI || evt.target === folderBgAI || evt.target === blankReal || evt.target === blankReal1 || evt.target === designTxt) {
+            console.log(blankReal + "a")
+            return
+        }
+        if (evt.target === leftFolder) {
+            designBg.style.display = "none";
+            designBgPix.style.display = "grid";
+            return;
+        }
+        if (evt.target === rightFolder) {
+            designBg.style.display = "none";
+            designBgAI.style.display = "grid";
+            return;
+        }
+
+        if (evt.target === leftFolderPix) {
+            designBg.style.display = "grid";
+            designBgPix.style.display = "none";
+            return;
+        }
+        if (evt.target === rightFolderPix) {
+            designBgPix.style.display = "none";
+            designBgAI.style.display = "grid";
+            return;
+        }
+        if (evt.target === leftFolderAI) {
+            designBgAI.style.display = "none";
+            designBgPix.style.display = "grid";
+            return;
+        }
+        if (evt.target === rightFolderAI) {
+            designBgAI.style.display = "none";
+            designBg.style.display = "grid";
+            return;
+        }
+
+        blankPlaceholder = evt.target.getAttribute("src");
+        modal.style.display = "initial";
+
+        try {
+            modalImg.src = blankPlaceholder;
+        } catch (SyntaxError) {
+            // do nothing if clicked on background or something
+        }
+    }
+
+    function poof() {
+        modal.style.display = "none";
+    }
+
+    gallery.addEventListener("click", modalAppear, false);
+    modal.addEventListener("click", poof);
+
+    const audioWind = document.getElementById("player");
+
+    let timeout;
+    document.onmousemove = function(){
+        clearTimeout(timeout);
+        timeout = setTimeout(function(){
+            audioWind.muted = false;
+            // window.alert("yeah")
+        }, 20000);
+        if (audioWind.muted === false) {
+            audioWind.muted = true;
+        }
+    }
 });
